f6af7ef05864c6f1e83fb2de847735a811446a5b
Maddie Black <madalyn.black@mx.com>
2022-07-14 23:38:04 -0600

    Initial commit CRAPP

---
+++ personal/sudoku/src/App.js
@@ -0,0 +1,13 @@
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles.component}>
+      <header>
+        Sudoku
+      </header>
+    </div>
+  );
+}
+
+export default App;
